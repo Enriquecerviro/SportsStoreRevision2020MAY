d9f6bf7f5845de1234411cda9bfa7bd6b9f90f41
Enriquecerviro <enrique.cervirodam@gmail.com>
2020-05-15 18:46:30 +0200

    Initial Project

---
+++ SportsStoreRevision2020MAY/Models/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreRevision2020MAY.Models
+{
+    /// <summary>
+    /// Necesitamos una manera de recojer Productos de la db, para ello definiremos una interfaz IQueryable para obtener una secuencia d eProcutos
+    /// 
+    /// </summary>
+    public class IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStoreRevision2020MAY/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreRevision2020MAY.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the product identifier.
+        /// </summary>
+        /// <value>
+        /// The product identifier.
+        /// </value>
+        public int ProductID { get; set; }
+        public string Name {get; set;}
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+
+}
